997a8023d3f64530eb48062c5a1b2051057ab559
Arthur Henrique <arthurvieira.henrique@gmail.com>
2018-07-24 23:32:00 -0300

    creating the project

---
+++ Xapo/Xapo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Xapo
+//
+//  Created by Arthur Henrique Vieira de Oliveira on 24/07/18.
+//  Copyright © 2018 Arthur Henrique Vieira de Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
