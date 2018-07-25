075d72cd9c85310495495f2d7c97c180d167ff08
Arthur Henrique <arthurvieira.henrique@gmail.com>
2018-07-24 23:44:59 -0300

    adding a custom view

---
+++ Xapo/Xapo/Objects/View.swift
@@ -0,0 +1,29 @@
+//
+//  ViewExt.swift
+//  XapoTest
+//
+//  Created by Arthur Henrique Vieira de Oliveira on 24/07/18.
+//  Copyright © 2018 Arthur Henrique Vieira de Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class View: UIView {
+    @IBInspectable var borderColor : UIColor = UIColor.clear {
+        didSet {
+            layer.borderColor = borderColor.cgColor
+        }
+    }
+    
+    @IBInspectable var borderWidth : CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var cornerRadius : CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+        }
+    }
+}
